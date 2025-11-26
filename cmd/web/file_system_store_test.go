991fc39c45d80bd6ffccc2b26b9916531b37dae4
AndreReyesG <jandre.reyesg@gmail.com>
2025-11-26 16:10:30 -0600

    test(file system store test): create a new helper function named createTempFile(), and store wins for existing players

--- cmd/web/file_system_store_test.go
+++ cmd/web/file_system_store_test.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"strings"
+	"io"
+	"os"
 	"testing"
 )
 
 func TestFileSystemStore(t *testing.T) {
 	t.Run("league from a reader", func(t *testing.T) {
-		database := strings.NewReader(`[
-		{"Name": "Moka", "Wins": 10},
-		{"Name": "Milky", "Wins": 33}]`)
-
+		database, cleanDatabase := createTempFile(t, `[
+			{"Name": "Moka", "Wins": 10},
+			{"Name": "Milky", "Wins": 33}]`)
+		defer cleanDatabase()
 		store := FileSystemPlayerStore{database}
 
 		got := store.GetLeague()
@@ -28,9 +29,10 @@ func TestFileSystemStore(t *testing.T) {
 	})
 
 	t.Run("get player score", func(t *testing.T) {
-		database := strings.NewReader(`[
-		{"Name": "Moka", "Wins": 10},
-		{"Name": "Milky", "Wins": 33}]`)
+		database, cleanDatabase := createTempFile(t, `[
+			{"Name": "Moka", "Wins": 10},
+			{"Name": "Milky", "Wins": 33}]`)
+		defer cleanDatabase()
 
 		store := FileSystemPlayerStore{database}
 
@@ -39,6 +41,40 @@ func TestFileSystemStore(t *testing.T) {
 		assertScoreEquals(t, got, want)
 
 	})
+
+	t.Run("store wins for existing players", func(t *testing.T) {
+		database, cleanDatabase := createTempFile(t, `[
+			{"Name": "Moka", "Wins": 10},
+			{"Name": "Milky", "Wins": 33}]`)
+		defer cleanDatabase()
+
+		store := FileSystemPlayerStore{database}
+
+		store.RecordWin("Milky")
+
+		got := store.GetPlayerScore("Milky")
+		want := 34
+		assertScoreEquals(t, got, want)
+
+	})
+}
+
+func createTempFile(t testing.TB, initialData string) (io.ReadWriteSeeker, func()) {
+	t.Helper()
+
+	tmpfile, err := os.CreateTemp("", "db")
+	if err != nil {
+		t.Fatalf("could not create temp file %v", err)
+	}
+
+	tmpfile.Write([]byte(initialData))
+
+	removeFile := func() {
+		tmpfile.Close()
+		os.Remove(tmpfile.Name())
+	}
+
+	return tmpfile, removeFile
 }
 
 func assertScoreEquals(t testing.TB, got, want int) {
