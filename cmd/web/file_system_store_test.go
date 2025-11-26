1d89df0a65bd55fd31f0096798a9f1fcd564ca31
AndreReyesG <jandre.reyesg@gmail.com>
2025-11-26 15:10:18 -0600

    test: get league from a reader

---
+++ cmd/web/file_system_store_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestFileSystemStore(t *testing.T) {
+	t.Run("league from a reader", func(t *testing.T) {
+		database := strings.NewReader(`[
+		{"Name": "Moka", "Wins": 10},
+		{"Name": "Milky", "Wins": 33}]`)
+
+		store := FileSystemPlayerStore{database}
+
+		got := store.GetLeague()
+
+		want := []Player{
+			{"Moka", 10},
+			{"Milky", 33},
+		}
+
+		assertLeague(t, got, want)
+	})
+}
