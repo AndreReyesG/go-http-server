32d308ff09eabd4479d3bdffb0c6d09d004ea6a3
AndreReyesG <jandre.reyesg@gmail.com>
2025-11-29 15:39:32 -0600

    feat(cli): use bufio.Scanner to read the input from the io.Reader

--- cmd/cli/CLI.go
+++ cmd/cli/CLI.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"bufio"
+	"io"
+	"strings"
+
 	"poker/internal/data"
 )
 
 type CLI struct {
 	store data.PlayerStore
+	in    io.Reader
 }
 
 func (cli *CLI) PlayPoker() {
-	cli.store.RecordWin("Milky")
+	reader := bufio.NewScanner(cli.in)
+	reader.Scan()
+	cli.store.RecordWin(extractWinner(reader.Text()))
+}
+
+func extractWinner(userInput string) string {
+	return strings.Replace(userInput, " wins", "", 1)
 }
