692f176284ed72a026aaebcb3b8c3f4d9535ef33
AndreReyesG <jandre.reyesg@gmail.com>
2025-11-29 15:41:12 -0600

    feat(cli): wire up into main

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"poker/internal/store"
+)
+
+const dbFileName = "game.db.json"
 
 func main() {
 	fmt.Println("Let's play poker")
+	fmt.Println("Type {name} wins to record a win")
+
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatalf("problem opening %s %v", dbFileName, err)
+	}
+
+	store, err := store.NewFileSystemPlayerStore(db)
+	if err != nil {
+		log.Fatalf("problem creating the file system player store, %v", err)
+	}
+
+	game := CLI{store, os.Stdin}
+	game.PlayPoker()
 }
