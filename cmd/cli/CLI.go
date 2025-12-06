f8883868b70d7b5507cdfc6ff52126da2000edb9
AndreReyesG <jandre.reyesg@gmail.com>
2025-12-06 00:30:44 -0600

    feat(cli): create a new BlindAlerter interface and using it in the CLI struct

--- cmd/cli/CLI.go
+++ cmd/cli/CLI.go
@@ -4,23 +4,37 @@ import (
 	"bufio"
 	"io"
 	"strings"
+	"time"
 
 	"poker/internal/data"
 )
 
-type CLI struct {
-	store data.PlayerStore
-	in    *bufio.Scanner
+type BlindAlerter interface {
+	ScheduleAlertAt(duration time.Duration, amount int)
 }
 
-func NewCLI(store data.PlayerStore, in io.Reader) *CLI {
+type CLI struct {
+	store   data.PlayerStore
+	in      *bufio.Scanner
+	alerter BlindAlerter
+}
+
+func NewCLI(store data.PlayerStore, in io.Reader, alerter BlindAlerter) *CLI {
 	return &CLI{
-		store: store,
-		in:    bufio.NewScanner(in),
+		store:   store,
+		in:      bufio.NewScanner(in),
+		alerter: alerter,
 	}
 }
 
 func (cli *CLI) PlayPoker() {
+	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
+	blindTime := 0 * time.Second
+	for _, blind := range blinds {
+		cli.alerter.ScheduleAlertAt(blindTime, blind)
+		blindTime = blindTime + 10*time.Minute
+	}
+
 	userInput := cli.readLine()
 	cli.store.RecordWin(extractWinner(userInput))
 }
